76e75699fa278cc99c6ad834e33aff8f6f4983b8
blacksonic <soos.gabor86@gmail.com>
2017-04-02 18:56:37 +0200

    update generated files

--- src/backend-expectation.js
+++ src/backend-expectation.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("@angular/http");
 function stringifyBody(body) {
     return typeof body === 'string'
